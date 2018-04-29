45aa4f9f21c95150f3798c1ebcb7670da83dc696
Tarun <tshedhani@gmail.com>
2018-04-29 15:34:14 -0400

    Added configuration for Spring security for Feign Client

--- FeignClientService/src/main/java/com/example/proxy/LimitServiceProxy.java
+++ FeignClientService/src/main/java/com/example/proxy/LimitServiceProxy.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "ConfigClientService", url = "http://localhost:8090") 
+@FeignClient(name = "ConfigClientService", url = "http://localhost:8090", configuration = com.example.config.FeignClientConfiguration.class) 
 public interface LimitServiceProxy {
 
 //	@GetMapping("/version/getName/{firstName}/{lastName}")
